59f90660d715fa30416b6fab723e51ff530aaf2d
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-02-11 15:03:41 +0100

    Update Structures basic2 (how old).c
    Can later update that to a proper counter of age, with years months and days

--- Structures basic2.c
+++ Structures basic2.c
@@ -6,24 +6,22 @@ struct date {
     int day;
 };
 
-//void yearsCount (struct date, struct date);
+void yearsCount (struct date, struct date);
 
 int main () {
     //! showMemory(start=65520)
     printf("This is a program that counts approximate age of a person (just for training structures)\n");
     struct date today;
-    printf("Enter today's date in the format \"year month day\", for example: 2024 08 29\n");
+    printf("Enter today's date in the format \"YYYY MM DD\"\n");
     scanf("%d %d %d", &today.year, &today.month, &today.day);
     struct date dateBirth;
     printf("Enter your date of birth: ");
     scanf("%d %d %d", &dateBirth.year, &dateBirth.month, &dateBirth.day);
-    //yearsCount(today.year, dateBirth.year);
-    printf("Your age is approximately %d\n", (today.year - dateBirth.year));
+    yearsCount(today, dateBirth);
     return 0;
 }
 
-/*
+
 void yearsCount (struct date a, struct date b) {
-    printf("Your age is approximately %d\n", (date.a - date.b));
+    printf("Your age is approximately %d\n", (a.year - b.year));
 }
-*/

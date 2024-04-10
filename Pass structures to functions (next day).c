4c05934d6064979c181273f3cfecd9b0fa639aaf
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-04-10 23:24:07 +0300

    Update Pass structures to functions (next day).c
    Will need an upgrade: count leap year differently, check user input, etc

--- Pass structures to functions (next day).c
+++ Pass structures to functions (next day).c
@@ -1,3 +1,5 @@
+//upgrade: count leap year differently, check user input, etc
+
 /*In this problem you will continue developing the data feature which you started implementing in the previous problem. You will implement a "tomorrow" feature in the C programming language via a function called "advanceDay()". The function advanceDay() should take as input a date (stored in a struct date) and return the date of the following day. You do not have to take into account leap years (although you may if you wish to). That is, it is okay for your function to always return March 1 as the day following February 28, no matter the year.
 
 You are provided with a familiar date structure definition, a main function as well as the function prototypes for the readDate(), printDate(), and advanceDay() functions. Do not modify any of the given code. Simply add your function definitions underneath the main() function. For the readDate() and printDate() functions you may simply copy and paste the code you developed in the previous task.

66758a7475f607f110bd532a430aef50da6e53dc
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-04-27 21:37:48 +0200

    Update Structs in functions (Date difference).c
    Fixed regex error by typecasting struct into string,, made canning user input and valiDate more efficient. To do:check the math, handle if user puts later date, then earlier date

--- Exercises of similar lever/Structs in functions (Date difference).c
+++ Exercises of similar lever/Structs in functions (Date difference).c
@@ -1,9 +1,11 @@
+ 
 //A program that counts how much days and DMY passed between two dates
 //inspired by my other program, that counts what day is next
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <regex.h>
+#include <math.h>
 
 struct date {
     int day;
@@ -23,35 +25,38 @@ int valiDate(struct date *);
 
 int main() {
     struct date date1, date2;
-    
     float avgDays = averageDays();
     
     printf("Enter the first date (Format: DD MM YYYY): ");
     readDate(&date1);
-    printf("Enter the second date (Format: DD MM YYYY): ");
-    readDate(&date2);
+    if (valiDate(&date1)) {
+        printf("Enter the second date (Format: DD MM YYYY): ");
+        readDate(&date2);
+    } else {
+        return 0;
+    }
     
     //error handling  
     //regex positive numbers 
-    if (valiDate(&date1) && valiDate(&date2)) {
+    if (valiDate(&date2)) {
         //if user input 33 14 2024
         if (!((date1.day == 29 && date1.month == 2 && isLeapYear(date1.year))
         || (date2.day == 29 && date2.month == 2 && isLeapYear(date2.year)))) {
             if ((date1.day > daysInMonth[date1.month -1]) || (date2.day > daysInMonth[date2.month -1])) {
-                printf("This date doesn't exist\n");
+                fprintf(stderr, "This date doesn't exist\n");
                 return 0;
             }
         }
       
         int daysPassedInt = daysPassedFunct(&date1, &date2);
         if (daysPassedInt < 0) {
-            printf("Error: negative number\n");
+            fprintf(stderr, "Error: negative number\n");
             return 0;
         }
         
-        printf("the first date: ");
+        printf("\nThe first date: ");
         printDate(date1);
-        printf("the second date: ");
+        printf("The second date: ");
         printDate(date2);
         
         printf("\n\nBetween these dates passed: %d days\n", daysPassedInt);
@@ -98,11 +103,9 @@ int daysPassedFunct(struct date *date1, struct date *date2) {
     //diff month, 1>2
     if (date1->month > date2->month && date1->year < date2->year) { //year1 is strictly more than year2
         daysPassed += daysInMonth[date1->month -1] - date1->day + date2->day;
-       //count I times
-       // int j = 12 - date1->month + date2->month;
-        
+       //count i times
         for (int i = date1->month; i < 12; i++) { //if month difference is >1
-            daysPassed += daysInMonth[i];        //count until dec
+            daysPassed += daysInMonth[i];         //count until dec
             //printf("daysPassed, loop2 count1: %d\n", daysPassed);
         }
         
@@ -143,13 +146,24 @@ void daysToYMD(int daysPassed, float averageDays) {
     daysPassed %= 365;
     printf("Years: %d\n", difference->year);
     
-    difference->month = (int)((float)daysPassed / averageDays);
-    printf("daystoYMD diff months: %d\n");
+    //difference->month = round((float)daysPassed / averageDays);
+    //printf("daystoYMD diff months rounded typecasting : %d\n", difference->month);
+    difference->month = daysPassed / 30;
+    
     daysPassed %= 30;
     printf("Months: %d\n", difference->month);
     
     difference->day = daysPassed;
     printf("Days: %d\n\n", difference->day);
+    
+    if (difference->year > 0) {
+        printf("It is %d years, %d months and %d days\n", difference->year, difference->month, difference->day);
+        printf("It is %d months and %d days\n", (difference->month + (difference->year *12)), difference->day);
+    }
+    else if (difference->year == 0) {
+        printf("It is %d months and %d days\n", difference->month, difference->day);
+    }
+
     free(difference);
 }
 
@@ -160,10 +174,46 @@ float averageDays () {
         x += 365;
     }
     x++; //29 Feb
-    y = ((float)x/48.0); //48 = 12 months * 4 years
+    y = ((float)x/48.0); //48 = 12 months * 4 years from the for loop
     printf("Average days in month, including leap year: %f\n\n", y);
     return y;
 }
+
+int valiDate(struct date *x) {
+    char xStr[11]; //lets say max 10 characters
+    regex_t regex;
+    int regexCompilation;
+    int regexExecution;
+
+    //sprintf to format the date components into a string buff
+    sprintf(xStr, "%02d %02d %04d", x->day, x->month, x->year);
+
+    regexCompilation = regcomp(&regex, "^(0[1-9]|[1-2][0-9]|3[0-1]) (0[1-9]|1[0-2]) [0-9]{4}$", REG_EXTENDED); //check if regex is correct
+
+    if (regexCompilation) {
+        fprintf(stderr, "Regex compilation failed\n");
+        return 0; 
+    }
+
+    //commpare the date input with the regex pattern
+    regexExecution = regexec(&regex, xStr, 0, NULL, 0); //only works with strings
+    regfree(&regex);
+
+    if (regexExecution == 0){
+        return 1;
+    }
+    else if (regexExecution == REG_NOMATCH) {
+        fprintf(stderr, "Invalid date format.\n");
+    } else {
+        fprintf(stderr, "Regex error...\n");
+    }
+
+    return 0;
+}
+
+
+    //ADD alternative time units - sec, min, h
+    
     
     /* add feature of swapping?
     struct date *temp = date1;
@@ -174,32 +224,4 @@ float averageDays () {
 
 //!!! add fgets
 
-int valiDate(struct date *x) {
-    regex_t regex;
-    int regexCompilation;
-    int regexExecution;
-
-    //compile the regex pattern
-    regexCompilation = regcomp(&regex, "^(0[1-9]|[1-2][0-9]|3[0-1]) (0[1-9]|1[0-2]) [0-9]{4}$", REG_EXTENDED); //check if regex is correct
-
-    if (regexCompilation) {
-        fprintf(stderr, "Regex compilation failed\n");
-        return 0; 
-    }
-
-
-    //commpare the date input with the regex pattern
-    regexExecution = regexec(&regex, x, 0, NULL, 0);
-    regfree(&regex);
-
-    if (regexExecution == 0){
-        return 1;
-    }
-    else if (regexExecution == REG_NOMATCH) {
-        fprintf(stderr, "Invalid date format.\n");
-    } else {
-        fprintf(stderr, "Error...\n");
-    }
-
-    return 0;
-}
+//add count only workdays?
